dad15109eadbff9fd7f8f1e3c478d41b5dd9999b
John Pinkerton <jpinkerton88@gmail.com>
2017-08-31 01:51:30 -0400

    remove console log lint ignores

--- src/index.js
+++ src/index.js
@@ -6,18 +6,18 @@ import { TLD } from "./settings";
 const server = createServer();
 
 server.listenUdp({ port: 9999, address: "127.0.0.1" }, () => {
-  console.log("DNS server started on port 9999"); // eslint-disable-line no-console
+  console.log("DNS server started on port 9999");
 });
 
 server.on("query", (query, done) => {
   const type = query.type();
   if (type !== "SRV") {
-    console.log(`${type} records are not supported`); // eslint-disable-line no-console
+    console.log(`${type} records are not supported`);
     query.respond();
     return;
   }
   const domain = query.name();
-  console.log(`DNS ${type} Query: ${domain}`); // eslint-disable-line no-console
+  console.log(`DNS ${type} Query: ${domain}`);
   const target = new SRVRecord(TLD, 3434);
   query.addAnswer(domain, target);
   query.respond();
